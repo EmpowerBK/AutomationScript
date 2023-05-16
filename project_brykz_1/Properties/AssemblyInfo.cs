d4da38ec85188cd5f082e88871484763766dd977
brykz <74079745+brykz@users.noreply.github.com>
2023-05-16 14:09:11 -0400

    Add project files.

---
+++ project_brykz_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("project_brykz_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("project_brykz_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("29C8A0E6-892C-4EB1-BCBF-6D96D80F64E3")]
+[assembly: AssemblyVersion("1.0.0.0")]
